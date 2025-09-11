67ac0be1d012b684cb89c7244b6e2d727081b0c9
ROG <luan.roger.2003@gmail.com>
2025-09-11 09:16:37 -0300

    refactor: update plugin import in example

--- examples/react-vite-ts/vite.config.ts
+++ examples/react-vite-ts/vite.config.ts
@@ -1,9 +1,10 @@
-import { wasmDirective } from "@rog/vite-plugin-use-wasm";
+/** biome-ignore-all lint/correctness/useHookAtTopLevel: This is not a React component */
+import useWasm from "@rog/vite-plugin-use-wasm";
 import Inspect from "vite-plugin-inspect";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [wasmDirective(), Inspect(), tailwindcss(), react()],
+  plugins: [useWasm(), Inspect(), tailwindcss(), react()],
 });
